2152fe3342d9c9149ececc7197aea4bc3cbcd2ff
Júlio César <juliocesardamasceno72@gmail.com>
2020-09-11 14:43:15 -0300

    Lutris installation project

---
+++ install_lutris.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Instalação automática do lutris.
+echo "-------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "Bem-Vindo! Você está prestes a instalar o lutris no seu Ubuntu ou derivado. Para continuar a instação aperte <ENTER> ou para cancelar, aperte <Ctrl-c>."
+echo "-------------------------------------------------------------------------------------------------------------------------------------------------------"
+read
+clear
+
+echo "-----------------------"
+echo "Instalando o lutris... "
+echo "-----------------------"
+sleep 5
+clear
+
+sudo add-apt-repository ppa:lutris-team/lutris -y &&  sudo apt-get update -y  &&  sudo apt-get install lutris -y
+
+clear
+echo "---------------------"
+echo "Instalação comcluída!"
+echo "---------------------"
+echo "Aperte <ENTER> para continuar... "
+read #pausa até que ENTER seja pressionado.
